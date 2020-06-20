bbf1c6cd99b60f6da79821ef3b2908d51d1d4614
frankda <36907108+frankda@users.noreply.github.com>
2020-06-21 09:02:59 +1000

    new post section done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CommentsList from './components/CommentsList/CommentsList';
+import NewPost from './components/NewPost/NewPost';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewPost />
+      <CommentsList />
     </div>
   );
 }
---
+++ src/components/NewPost/NewPost.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NewPost.css'
+
+export default () => {
+  return (
+    <div className="new-post">
+      <div className="new-post__content">
+        <p className="new-post__title">
+          Maths For 'em
+        </p>
+        <button className="new-post__btn btn">
+          <p className="new-post__btn__name">
+            New Post
+          </p>
+        </button>
+    </div>
+    </div>
+  );
+}
